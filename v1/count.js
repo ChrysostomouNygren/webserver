c4854d9c3601122e2670316bb276f3a432c9b91b
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-05-04 14:14:07 +0200

    week 1

--- v1/count.js
+++ v1/count.js
@@ -1,8 +1,8 @@
 // Uppgift:
 // https://github.com/cme-osuka/webbserverdatabaser/blob/main/week1/lecture2/exercises/count.md
 
-const process = require("process");
 const count = process.argv.splice(2);
+// Tar bort de 2 första onödiga argumenten i arrayen.
 
 
 console.log(`this is a few words`);
